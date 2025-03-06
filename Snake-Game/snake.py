cec0ae8ae999567a2d906b027bb8743ad8c8cbd1
Wunna Moe San <wannamoesan11@gmail.com>
2025-03-06 15:00:08 +0630

    Add files via upload

---
+++ Snake-Game/snake.py
@@ -0,0 +1,56 @@
+from turtle import Turtle
+
+STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.snake_create()
+
+    def snake_create(self):
+        for position in STARTING_POSITION:
+            new_segment = Turtle(shape='square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move_snake(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+
+    def extend_snake(self):
+        extended_blocks = Turtle(shape='square')
+        extended_blocks.color('white')
+        extended_blocks.penup()
+        extended_blocks.goto(self.segments[-1].position())
+        self.segments.append(extended_blocks)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(3000, 4000)
+        self.segments.clear()
+        self.snake_create()
+
+    def up(self):
+        if self.segments[0].heading() != 270:
+            self.segments[0].setheading(90)
+
+    def down(self):
+        if self.segments[0].heading() != 90:
+            self.segments[0].setheading(270)
+
+    def left(self):
+        if self.segments[0].heading() != 0:
+            self.segments[0].setheading(180)
+
+    def right(self):
+        if self.segments[0].heading() != 180:
+            self.segments[0].setheading(0)
+
+
